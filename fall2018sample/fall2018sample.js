f0d75f68b381330a1486a6e57842434c3bc447f8
Conrad Hindman (RIT Student) <cmh1026@ad.rit.edu>
2018-12-06 16:29:52 -0500

    fixed the homework

--- fall2018sample/fall2018sample.js
+++ fall2018sample/fall2018sample.js
@@ -1,1 +1,8 @@
 /* Don't use <script> tags in a linked js file! */
+$(".menuitem").click(function () {
+    $(this).next(".submenu").slideToggle("slow");
+        //$(".submenu").slideToggle("slow");
+    });
+
+    let content = "content1.txt";
+    $("#content").load(content);

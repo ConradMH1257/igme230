a2b3760c4709f2fcea41c1dc9a80178526812bdc
Conrad Hindman (RIT Student) <cmh1026@ad.rit.edu>
2018-12-06 16:51:26 -0500

    some added stuff

--- fall2018sample/fall2018sample.js
+++ fall2018sample/fall2018sample.js
@@ -5,4 +5,14 @@ $(".menuitem").click(function () {
     });
 
     let content = "content1.txt";
+    let cString = `${content}`;
     $("#content").load(content);
+
+    let content2 = "content2.txt";
+    let cString2 = `${content2}`;
+
+    $("#choose-content").change(function() {
+        let content2 = "content2.txt";
+        let cString2 = `${content2}`;
+        $("#content").load(cString2);
+    });

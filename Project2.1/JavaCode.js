af7a51b5ada013d2ab019e3682e53cdd61f67a19
ConradMH1257 <42787458+ConradMH1257@users.noreply.github.com>
2018-12-09 21:54:22 -0500

    Added files for testing

--- Project2.1/JavaCode.js
+++ Project2.1/JavaCode.js
@@ -1,3 +1,3 @@
-$(".menu").click(function () {
-    $(this).next(".items").slideToggle("slow");
+$(".menu").click(function () {//Calls the nav menu header
+    $(this).next(".items").slideToggle("slow");//checks to see if there is anything below the nav header
     });
